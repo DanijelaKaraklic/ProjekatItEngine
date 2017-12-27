927fbb8870942a2bcda340b9d52a9bfe38e94e65
Nikola Kosmajac <nikola.kosmajac@itengine.eu>
2017-12-27 15:16:04 +0100

    Izmenjen edit

--- app/src/main/java/com/example/nikolakosmajac/user/activities/EditActivity.java
+++ app/src/main/java/com/example/nikolakosmajac/user/activities/EditActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.nikolakosmajac.user.R;
@@ -27,35 +29,45 @@ public class EditActivity extends AppCompatActivity {
             setSupportActionBar(toolbar);
         }
 
-        EditText editId = (EditText)findViewById(R.id.id_user);
-        EditText editUsername = (EditText)findViewById(R.id.username_user);
-        EditText editPassword = (EditText)findViewById(R.id.pass_user);
-        EditText editName = (EditText)findViewById(R.id.name_user);
-        EditText editSurname = (EditText)findViewById(R.id.surname_user);
-        EditText editAdress = (EditText)findViewById(R.id.adress_user);
-        EditText editImage = (EditText)findViewById(R.id.image_user);
+        Button btn_update = (Button)findViewById(R.id.btn_update_ok);
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        int id = Integer.parseInt(editId.getText().toString());
-        String username = editUsername.getText().toString();
-        String password = editPassword.getText().toString();
-        String name = editName.getText().toString();
-        String surname = editSurname.getText().toString();
-        String adress = editAdress.getText().toString();
-        String image = editImage.getText().toString();
+                EditText editId = (EditText)findViewById(R.id.id_user);
+                EditText editUsername = (EditText)findViewById(R.id.username_user);
+                EditText editPassword = (EditText)findViewById(R.id.pass_user);
+                EditText editName = (EditText)findViewById(R.id.name_user);
+                EditText editSurname = (EditText)findViewById(R.id.surname_user);
+                EditText editAdress = (EditText)findViewById(R.id.adress_user);
+                EditText editImage = (EditText)findViewById(R.id.image_user);
+
+                int id = Integer.parseInt(editId.getText().toString());
+                String username = editUsername.getText().toString();
+                String password = editPassword.getText().toString();
+                String name = editName.getText().toString();
+                String surname = editSurname.getText().toString();
+                String adress = editAdress.getText().toString();
+                String image = editImage.getText().toString();
 
 
-        Intent intent = new Intent();
-        Bundle bundle = new Bundle();
-        bundle.putInt("id",id);
-        bundle.putString("username",username);
-        bundle.putString("password",password);
-        bundle.putString("name",name);
-        bundle.putString("surname",surname);
-        bundle.putString("adress",adress);
-        bundle.putString("image",image);
-        intent.putExtras(bundle);
-        setResult(RESULT_OK,intent);
-        finish();
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                bundle.putInt("id",id);
+                bundle.putString("username",username);
+                bundle.putString("password",password);
+                bundle.putString("name",name);
+                bundle.putString("surname",surname);
+                bundle.putString("adress",adress);
+                bundle.putString("image",image);
+                intent.putExtras(bundle);
+                setResult(RESULT_OK,intent);
+                finish();
+                
+            }
+        });
+
+
 
     }
 
